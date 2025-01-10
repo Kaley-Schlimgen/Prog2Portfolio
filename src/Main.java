a44480aa0c58531d566495aa59a876022d0fd338
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-01-10 12:55:08 -0700

    final

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,17 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //each student object stored in a linked list
         LinkedList<Student> studentData = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
         String addAnother = "yes";
 
-        while (addAnother.equalsIgnoreCase("yes")) {
+        try {
+            //Loop that prompts user for student data
+            while (addAnother.equalsIgnoreCase("yes")) {
 
-            System.out.println("ENTER STUDENT INFORMATION");
-            System.out.print("Name: ");
+                System.out.println("ENTER STUDENT INFORMATION");
+                System.out.print("Name: ");
                 String name = scanner.nextLine();
 
                 System.out.print("Address: ");
@@ -28,33 +31,41 @@ public class Main {
 
                 studentData.add(new Student(name, address, GPA));
 
-            System.out.print("Do you have a student to add (yes/no)? ");
-            addAnother = scanner.nextLine();
+                System.out.print("Do you have a student to add (yes/no)? ");
+                addAnother = scanner.nextLine();
             }
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
         scanner.close();
 
         //Sort linkedList by name in ascending order
         Collections.sort(studentData, Comparator.comparing(Student::getName));
 
-        System.out.println("\nSTUDENT INFORMATION: ");
+        //Output student data
+/*        System.out.println("\nSTUDENT INFORMATION: ");
         for (Student student : studentData) {
             System.out.println("\nName: " + student.getName() + "\nAddress: " + student.getAddress() +
                     "\nGPA: " + student.getGPA());
         }
+*/
+        System.out.println("\nSTUDENT INFO SUCCESSFULLY WRITTEN TO FILE: outputContents.txt");
 
         writeToFile("outputContents.txt", studentData);
-        }
+    }
 
+    //Also output student data to a regular text file
     private static void writeToFile(String file, LinkedList<Student> studentData) throws IOException {
         FileWriter writer = new FileWriter(file);
+        writer.write("STUDENT INFORMATION:\n");
         for (Student student : studentData) {
-            writer.write(student.getName() + ", " + student.getAddress() + ", " + student.getGPA() + "\n");
+            writer.write("\nName: " + student.getName() + "\nAddress: " + student.getAddress() +
+                    "\nGPA: " + student.getGPA() + "\n");
         }
         writer.close();
 
-
     }
-    }
+}
 
 
--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,25 @@
 
 public class Student {
-    //student data as private fields (name, address, GPA)
+    //Student data as private fields (name, address, GPA)
     private String name;
     private String address;
     private double GPA;
 
-    public Student(String name, String address, double GPA) {
+    public Student(String name, String address, double GPA) throws IllegalAccessException {
         this.name = name;
         this.address = address;
-        this.GPA = GPA;
+        validateGPA(GPA);
+        //this.GPA = GPA;
+    }
+
+    //Validate numeric data for GPA
+    public void validateGPA(double GPA) throws IllegalAccessException {
+        if (GPA >= 0.0 && GPA <= 4.0) {
+            this.GPA = GPA;
+        }
+        else {
+            throw new IllegalAccessException("GPA not valid");
+        }
     }
 
     public String getName() {
